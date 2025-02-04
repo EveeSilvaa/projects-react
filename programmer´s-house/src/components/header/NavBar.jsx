f2c1a8ec79bd94f8d319ae32b9b72e7f81e6a99f
evellyn <maevee38@gmail.com>
2025-02-04 13:47:28 -0300

    excluding erros

---
+++ programmer´s-house/src/components/header/NavBar.jsx
@@ -0,0 +1,19 @@
+
+
+function NavBar() {
+  return (
+    <div className="nav-container">
+    <header>
+        <nav>
+            <ul className="nav-list">
+                <a href="/Home">Home</a>
+                <a href="/about">About</a>
+                <a href="/services">Services</a>
+            </ul>
+        </nav>
+    </header>
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ programmer´s-house/src/components/rodapé/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <Footer className="footer">
+        <div>
+            <p>Desenvolvido por: <a href="https://www.linkedin.com/in//">evellyn silva</a></p>
+        </div>
+    </Footer>
+  )
+}
+
+export default Footer;
