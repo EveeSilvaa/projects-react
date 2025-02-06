666d6df929e26ecf90a7ed27b6e4061e34883886
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-02-06 15:29:29 -0300

    new configs

---
+++ wiki-repository/src/components/ItemRepo/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        color: #FAFAFA;
+        font-size: 32px;
+    }
+
+    P {
+        color: #FAFAFA60;
+        font-size: 16px;
+        margin-top: 20px;
+    }
+    
+    a {
+        color: #FF0000;
+        padding: 20px;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
